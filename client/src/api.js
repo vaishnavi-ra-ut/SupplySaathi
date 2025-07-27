12e2456fc57a38e77ac8ce78e75f9b2e367174b5
Vaishnavi Raut <rautvaishnavi29@gmail.com>
2025-07-28 02:58:16 +0530

    updates

--- client/src/api.js
+++ client/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-  baseURL: 'http://localhost:7777/api',
+  baseURL: 'https://supplysaathi-3.onrender.com/',
   withCredentials: true,
 });
 export default api; 
