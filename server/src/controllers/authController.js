e749c585c904b477ab6e956df0b0033a406f5209
Vaishnavi Raut <rautvaishnavi29@gmail.com>
2025-07-30 21:33:18 +0530

    Removed Bug

--- server/src/controllers/authController.js
+++ server/src/controllers/authController.js
@@ -5,7 +5,7 @@ const jwt = require('jsonwebtoken');
 const cookieOptions = {
   httpOnly: true,
   secure: process.env.NODE_ENV === 'production',
-  sameSite: 'lax',
+  sameSite: 'None',
   maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
 };
 
